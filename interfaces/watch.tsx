6545830bc59ba8a3ca342de2a9a660a9ebd7587b
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-09-24 22:47:32 -0400

    interfaces/: crearon las interfaces

---
+++ interfaces/watch.tsx
@@ -0,0 +1,13 @@
+export interface IWatch {
+    title: string;
+    description: string;
+    tags: string[];
+    videos: string[];
+    slug: string;
+    image: string,
+    inLike: number;
+    inSend: number;
+    inSave: number;
+    inShare: number;
+}
+
