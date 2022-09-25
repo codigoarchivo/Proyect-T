319cd0b999641fdfdaea533f3586720a9c5c09cf
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-09-24 22:39:52 -0400

    pages/index.tsx: agrego componente Swipers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import type { NextPage } from 'next'
 
 import { TyerraLayout } from '../components/layouts'
 
+import { Swipers } from '../components/ui'
+
 const HomePage: NextPage = () => {
   return (
     <TyerraLayout title={'Home'} pageDescription={'Pagina de inicio'}>
-      <h1>Hola Mundo</h1>
+       <Swipers/>
     </TyerraLayout>
   )
 }
